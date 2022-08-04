8d2aa52b8fb3ae47a76f7500d4a56fe36ecfd8c6
Chetuw <wagadarechetan@gmail.com>
2022-08-04 13:11:59 +0530

    Done

--- src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -2,6 +2,7 @@ package com.myapp.spring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
@@ -10,10 +11,10 @@ class DevopsGroovyProductsApplication {
 	static void main(String[] args) {
 		SpringApplication.run(DevopsGroovyProductsApplication, args)
 	}
-
+	
 	@GetMapping
 	def sayHello() {
 		return "Hello"
 	}
-}
 
+}
