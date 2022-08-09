170d84e2fa0bad4efa1e8e0ba30b5a27f556a450
Chetuw <wagadarechetan@gmail.com>
2022-08-09 23:30:51 +0530

    Changes updated

--- src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
-class DevopsGroovyProductsApplication {
+class DevopsGroovyProductsApplication extends SpringBootServletInitializer{
 
 	static void main(String[] args) {
 		SpringApplication.run(DevopsGroovyProductsApplication, args)
